9fcc2fc005c85194c155e6dfafd5a1e4de1de001
Natalie Wong <wongn341@gmail.com>
2020-04-04 21:51:59 -0400

    Added using statements and an event for buttonOpenDepartmentJSONFile

--- PayrollGUI/MainWindow.xaml.cs
+++ PayrollGUI/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Payroll;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +24,49 @@ namespace PayrollGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Department department = new Department();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void buttonOpenDepartmentJSONFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            var openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                textBoxDepartmentFilename.Text = openFileDialog.FileName;
+                textBoxDepartmentName.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            }
+
+            string fileName = textBoxDepartmentFilename.Text;
+            department = DeserializeDepartmentJSON(fileName);
+
+            string departmentName = department.Name;
+            textBoxDepartmentName.Text = departmentName;
+        }
+
+        /// <summary>
+        /// Reads Department data from JSON file <see cref="Department"/>
+        /// </summary>
+        /// <returns></returns>
+        public static Department DeserializeDepartmentJSON(string fileName)
+        {
+            Department department;
+
+            var reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            var inputSerializer = new DataContractJsonSerializer(typeof(Department));
+
+            department = (Department)inputSerializer.ReadObject(reader);
+            reader.Close();
+
+            return department;
+        }
     }
+
+    
 }
