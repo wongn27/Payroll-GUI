e77a1ae8df8bae9a740de042519f1dc9ba16c9f2
Natalie Wong <wongn341@gmail.com>
2020-04-15 15:43:30 -0400

    Used a converter in the Date column of the Shift ListView to show a formatted date (without the time),  added comments, added the if statement if the user chooses cancel in the open file dialog

--- PayrollGUI/MainWindow.xaml.cs
+++ PayrollGUI/MainWindow.xaml.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.Win32;
+﻿//******************************************************
+// File: MainWindow.xaml.cs
+//
+// Purpose: This application will use a Windows Presentation Foundation (WPF) GUI to display payroll data.
+//
+// Written By: Natalie Wong
+//
+// Compiler: Visual Studio 2019
+//
+//******************************************************
+
+using Microsoft.Win32;
 using Payroll;
 using System;
 using System.Collections.Generic;
@@ -31,6 +42,12 @@ namespace PayrollGUI
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Open Department JSON File button. When the user presses this button it
+        /// should display an open file dialog and let the user select the file to open.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonOpenDepartmentJSONFile_Click(object sender, RoutedEventArgs e)
         {
             // Create OpenFileDialog
@@ -43,6 +60,13 @@ namespace PayrollGUI
 
             var result = openFileDialog.ShowDialog();
 
+            // If the user chooses cancel, return to the window 
+            if (result == false)
+                return;
+
+            // If the user chooses to open the file then it should populate the department controls with data from the selected
+            // file, clear the worker TextBoxes (the controls used by Find Worker) and the filename should also appear in the 
+            // department filename TextBox in the main window
             if (result.HasValue && result.Value)
             {
                 textBoxDepartmentFilename.Text = openFileDialog.FileName;
@@ -58,9 +82,9 @@ namespace PayrollGUI
 
                 PopulateDepartmentAndTotalTextBoxes();
 
-                textBoxTargetWorkerId.Clear();
-
                 ClearWorkerTextBoxes(windowGrid, "textBoxWorker");
+
+                textBoxTargetWorkerId.Clear();
             }
             catch (Exception exception)
             {
@@ -78,12 +102,19 @@ namespace PayrollGUI
             }
         }
 
+        /// <summary>
+        /// Find Worker. When the user presses this button it should get data for the
+        /// target worker id and populate the TextBoxes to the right.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonFindWorker_Click(object sender, RoutedEventArgs e)
         {
             int targetWorkerId = Int32.Parse(textBoxTargetWorkerId.Text);
 
             Worker worker = department.FindWorker(targetWorkerId);
 
+            // If the worker id is not found the TextBoxes should just be cleared
             if (null == worker)
             {
                 ClearWorkerTextBoxes(windowGrid, "textBoxWorker");
@@ -111,6 +142,11 @@ namespace PayrollGUI
             return department;
         }
 
+        /// <summary>
+        /// Clears the Worker TextBoxes populated by the Find Worker button.
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <param name="startsWith"></param>
         public void ClearWorkerTextBoxes(Grid grid, string startsWith)
         {
             foreach (System.Windows.Controls.Control control in windowGrid.Children)
@@ -123,12 +159,20 @@ namespace PayrollGUI
             }
         }
 
+        /// <summary>
+        /// Populates a TextBox with data.
+        /// </summary>
+        /// <param name="textBoxString"></param>
+        /// <param name="textBox"></param>
         public void PopulateTextBox(string textBoxString, TextBox textBox)
         {
             textBox.Text = textBoxString;
             textBox.IsReadOnly = true;
         }
 
+        /// <summary>
+        /// Populates the Department Name, Total Worker Hours and the Total Worker Pay TextBoxes with data.
+        /// </summary>
         public void PopulateDepartmentAndTotalTextBoxes()
         {
             PopulateTextBox(department.Name, textBoxDepartmentName);
@@ -136,6 +180,12 @@ namespace PayrollGUI
             PopulateTextBox(department.CalculateTotalPay().ToString(), textBoxTotalWorkerPay);
         }
 
+        /// <summary>
+        /// Populates the Worker Name, Worker Id, Worker Pay Rate, Worker Hours and the Worker Pay Textboxes 
+        /// with the Target Worker Id's data.
+        /// </summary>
+        /// <param name="worker"></param>
+        /// <param name="targetWorkerId"></param>
         public void PopulateWorkerTextBoxes(Worker worker, int targetWorkerId)
         {
             PopulateTextBox(worker.Name, textBoxWorkerName);
